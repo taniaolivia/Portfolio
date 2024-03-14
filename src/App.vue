4366f06237cd5721b0fbc43414e5a236296bc922
Tania OLIVIA <tolivia@relatia.fr>
2024-03-14 18:02:29 +0100

    add joystick

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="map"></div>
+  <div id="map">
+  
+  </div>
+
+  <div id="mobileInterface" class="noSelect">
+        <div id="joystickWrapper1"></div>
+        
+      </div>
 </template>
 
 <script setup>
@@ -7,8 +14,15 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { onMounted } from 'vue';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import nipplejs from 'nipplejs';
 
-let mixer, pointingMixer, pointingModel, wave, walkModel, walk, point, mapModel, bridge1, bridge2, bridge3, bridge4;
+let pointingMixer, pointingModel, point;
+let mixer, walkModel, walk;
+let wave, mapModel, bridge1, bridge2, bridge3, bridge4;
+let arrowLeftModel, pointingArrowLeft, arrowLeftMixer;
+let arrowRightModel, pointingArrowRight, arrowRightMixer;
+let arrowFrontModel, pointingArrowFront, arrowFrontMixer;
 
 let cdn = import.meta.env.VITE_CDN_URL;
 let width = window.innerWidth, height = window.innerHeight;
@@ -21,8 +35,7 @@ let renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( width, height );
 renderer.gammaOutput = true;
 
-camera.position.set(-80, 10, 125);
-camera.rotation.y = -0.6;
+camera.position.set(-35, 50, 70);
 
 const leftLight = new THREE.DirectionalLight(0xffffff, 2.5);
 leftLight.position.set(-1, 0, 0);
@@ -48,74 +61,50 @@ const directionalLight4 = new THREE.DirectionalLight(0xffffff, 2.5);
 directionalLight4.position.set(0, 0, -1); 
 scene.add(directionalLight4);
 
+let controls = new OrbitControls( camera, renderer.domElement );
+
+let fwdValue = 0;
+let bkdValue = 0;
+let rgtValue = 0;
+let lftValue = 0;
+let tempVector = new THREE.Vector3();
+let upVector = new THREE.Vector3(0, 1, 0);
+let joyManager;
+
+controls.maxDistance = 15;
+controls.minDistance = 15;
+controls.maxPolarAngle = Math.PI/2 ;
+controls.minPolarAngle = 0;
+controls.autoRotate = false;
+controls.autoRotateSpeed = 0;
+controls.rotateSpeed = 0.4;
+controls.enableDamping = false;
+controls.dampingFactor = 0.1;
+controls.enableZoom = false;
+controls.enablePan = false;
+controls.minAzimuthAngle = - Math.PI/2; 
+controls.maxAzimuthAngle = Math.PI/4
+
+controls.update();
+
+let loader = new GLTFLoader();
+
+let dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( cdn + 'examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+
 onMounted(() => {
 
     document.getElementById("map").appendChild(renderer.domElement);
 
-    const loader = new GLTFLoader();
+    createModels();
 
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( cdn + 'examples/jsm/libs/draco/' );
-    loader.setDRACOLoader( dracoLoader );
-
-    loader.load(cdn + 'models/tania_pointing.glb', function ( gltf ) {
-    
-      pointingModel = gltf.scene;
-      pointingModel.scale.set(2, 2, 2); 
-      pointingModel.position.set(-70, 2, 110); 
-      pointingModel.rotation.set(0, -3.5, 0);
-
-      scene.add(pointingModel)
-      
-      pointingMixer = new THREE.AnimationMixer(pointingModel);
-      point = pointingMixer.clipAction(gltf.animations[0]);
-
-      pointingModel.visible = false;
-    });
-
-    loader.load(cdn + 'models/tania_walking.glb', function (gltf) {
-
-      walkModel = gltf.scene;
-      walkModel.scale.set(2, 2, 2); 
-      walkModel.position.set(-70, 2, 110); 
-      walkModel.rotation.set(0, -3.5, 0);
-
-      scene.add(walkModel);
-
-      mixer = new THREE.AnimationMixer(walkModel);
-
-      walk = mixer.clipAction(gltf.animations[0]);
-    })
-
-  
-    loader.load(cdn + 'models/map.glb', function ( gltf ) {
-
-        mapModel = gltf.scene;
-        mapModel.scale.set(0.5, 0.5, 0.5); 
-        mapModel.position.set(0, 0, 0); 
-        mapModel.rotation.set(0, 1, 0);
-        
-        scene.add(mapModel);
-
-        wave = mapModel.children[0];
-        bridge1 = mapModel.children[63];
-        bridge2 = mapModel.children[64];
-        bridge3 = mapModel.children[66];
-        bridge4 = mapModel.children[67];
-
-        /*const light = new THREE.PointLight( 0xffffff, 1, 100 );
-        light.position.set( 50, 50, 50 );
-        scene.add( light );*/
-      },
-      
-    );
-
-    document.onkeydown = ((event) => {
+    /*document.onkeydown = ((event) => {
       if(event.keyCode === 38) {
         walk.play();
 
         walkModel.translateZ(0.6);
-        walkModel.translateX(-0.1);
+        walkModel.translateX(-0.15);
 
         if (detectCollision(walkModel, bridge1) === true && walkModel.position.y <= (bridge1.position.y + bridge1.geometry.boundingBox.max.y) / 4) {
             walkModel.position.y = walkModel.position.y + (bridge1.position.y + bridge1.geometry.boundingBox.max.y) / 50; 
@@ -136,15 +125,35 @@ onMounted(() => {
         camera.position.copy(walkModel.position)
         camera.position.add(new THREE.Vector3(-17, 20, 30));
         camera.lookAt(walkModel.position)
+
       }
-      /*else if(event.keyCode === 39) {
+      else if(event.keyCode === 39) {
         walk.play();
-        walkModel.position.z -= 0.03;
-        walkModel.position.x += 0.03;
+
+        walkModel.translateZ(0.6);
+        walkModel.position.x += 0.1;
+        walkModel.rotation.y = 0.02;
+
+        if (detectCollision(walkModel, bridge1) === true && walkModel.position.y <= (bridge1.position.y + bridge1.geometry.boundingBox.max.y) / 4) {
+            walkModel.position.y = walkModel.position.y + (bridge1.position.y + bridge1.geometry.boundingBox.max.y) / 50; 
+        }
+        else if (detectCollision(walkModel, bridge2) === true && walkModel.position.y <= (bridge2.position.y + bridge2.geometry.boundingBox.max.y) / 2.2) {
+          walkModel.position.y = walkModel.position.y + (bridge2.position.y + bridge2.geometry.boundingBox.max.y) / 50; 
+        }
+        else if (detectCollision(walkModel, bridge3) === true && walkModel.position.y <= (bridge3.position.y + bridge3.geometry.boundingBox.max.y) / 2.2) {
+          walkModel.position.y = walkModel.position.y + (bridge3.position.y + bridge3.geometry.boundingBox.max.y) / 50; 
+        }
+        else if(detectCollision(walkModel, bridge1) === false && detectCollision(walkModel, bridge2) === false && detectCollision(walkModel, bridge3) === false && detectCollision(walkModel, bridge4) === true){
+            walkModel.position.y = walkModel.position.y - (bridge4.position.y + bridge4.geometry.boundingBox.max.y) / 40;
+        }
+        else if(detectCollision(walkModel, bridge1) === false && detectCollision(walkModel, bridge2) === false && detectCollision(walkModel, bridge3) === false && detectCollision(walkModel, bridge4) === false) {
+            walkModel.position.y = 2;
+        }
 
         camera.position.copy(walkModel.position)
-        camera.position.add(new THREE.Vector3(-15, 8, 20)); // Adjust distance from the model
+        camera.position.add(new THREE.Vector3(-17, 20, 30));
         camera.lookAt(walkModel.position)
+
       }
       else if(event.keyCode === 37) {
         walk.play();
@@ -154,7 +163,7 @@ onMounted(() => {
         camera.position.copy(walkModel.position)
         camera.position.add(new THREE.Vector3(-15, 8, 20)); // Adjust distance from the model
         camera.lookAt(walkModel.position)
-      }*/
+      }
       else {        
         point.play();
 
@@ -169,6 +178,9 @@ onMounted(() => {
       if(event.keyCode === 38) {
         walk.stop();
       }
+      else if(event.keyCode === 39) {
+        walk.stop();
+      }
       else {
         point.stop();
 
@@ -177,20 +189,146 @@ onMounted(() => {
 
         pointingModel.position.copy(walkModel.position)
       }
-    })
+    })*/
+
+    resize();
 
     animate();
+
+    window.addEventListener('resize',resize);
+
+    addJoystick();
 })
+
+function createModels() {
+
+    loader.load(cdn + 'models/map.glb', function ( gltf ) {
+
+        mapModel = gltf.scene;
+        mapModel.scale.set(0.5, 0.5, 0.5); 
+        mapModel.position.set(0, 0, 0); 
+        mapModel.rotation.set(0, 1, 0);
+        
+        scene.add(mapModel);
+
+        wave = mapModel.children[0];
+        bridge1 = mapModel.children[53];
+        bridge2 = mapModel.children[54];
+        bridge3 = mapModel.children[55];
+        bridge4 = mapModel.children[56];
+
+        console.log(mapModel)
+
+        /*const light = new THREE.PointLight( 0xffffff, 1, 100 );
+        light.position.set( 50, 50, 50 );
+        scene.add( light );*/
+
+        loader.load(cdn + 'models/tania_pointing.glb', function ( gltf ) {
+      
+          pointingModel = gltf.scene;
+          pointingModel.scale.set(0.7, 0.7, 0.7); 
+          pointingModel.position.set(-56.5, 6.5, 85); 
+          pointingModel.rotation.set(0, -3.5, 0);
+
+          scene.add(pointingModel)
+          
+          pointingMixer = new THREE.AnimationMixer(pointingModel);
+          point = pointingMixer.clipAction(gltf.animations[0]);
+
+          pointingModel.visible = false;
+        });
+
+        loader.load(cdn + 'models/tania_walking.glb', function (gltf) {
+
+          walkModel = gltf.scene;
+          walkModel.scale.set(0.7, 0.7, 0.7); 
+          walkModel.position.set(-56.5, 6.5, 85); 
+          walkModel.rotation.set(0, -3.5, 0);
+
+          scene.add(walkModel);
+
+          mixer = new THREE.AnimationMixer(walkModel);
+
+          walk = mixer.clipAction(gltf.animations[0]);
+        })
+
+        loader.load(cdn + 'models/arrowLeft.glb', function ( gltf ) {
+
+          arrowLeftModel = gltf.scene;
+          arrowLeftModel.scale.set(1, 1, 1); 
+          arrowLeftModel.position.set(-12, 5, -50);
+          arrowLeftModel.rotation.set(0, 1, 0);
+
+          scene.add(arrowLeftModel);
+
+          arrowLeftMixer = new THREE.AnimationMixer(arrowLeftModel);
+          pointingArrowLeft = arrowLeftMixer.clipAction(gltf.animations[0]);
+
+          pointingArrowLeft.play();
+
+          }); 
+
+        loader.load(cdn + 'models/arrowRight.glb', function ( gltf ) {
+
+          arrowRightModel = gltf.scene;
+          arrowRightModel.scale.set(1, 1, 1); 
+          arrowRightModel.position.set(15, 5, -70);
+          arrowRightModel.rotation.set(0, -2.1, 0);
+
+          scene.add(arrowRightModel);
+
+          arrowRightMixer = new THREE.AnimationMixer(arrowRightModel);
+          pointingArrowRight = arrowRightMixer.clipAction(gltf.animations[0]);
+
+          pointingArrowRight.play();
+
+          }); 
+
+        loader.load(cdn + 'models/arrowFront.glb', function ( gltf ) {
+
+          arrowFrontModel = gltf.scene;
+          arrowFrontModel.scale.set(2, 2, 2); 
+          arrowFrontModel.position.set(-25.5, 0, 40); 
+          arrowFrontModel.rotation.set(0, -2.1, 0);
+
+          scene.add(arrowFrontModel);
+
+          arrowFrontMixer = new THREE.AnimationMixer(arrowFrontModel);
+          pointingArrowFront = arrowFrontMixer.clipAction(gltf.animations[0]);
+
+          pointingArrowFront.play();
+        }); 
+    });
+}
+
+function resize(){
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  
+  renderer.setSize(w,h);
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
+}
+
 
 function animate( ) {
     requestAnimationFrame(animate);
     
-    if(mixer !== undefined) {
-      mixer.update(0.06);
+    if(walkModel !== undefined) {
+
+      updatePlayer();
+  
     }
 
-    if(pointingMixer !== undefined) {
+    if(mixer !== undefined && pointingMixer !== undefined) {
+      mixer.update(0.06);
       pointingMixer.update(0.04);
+    }
+
+    if(arrowLeftMixer !== undefined && arrowRightMixer !== undefined && arrowFrontMixer !== undefined) {
+      arrowLeftMixer.update(0.025);
+      arrowRightMixer.update(0.025);
+      arrowFrontMixer.update(0.025);
     }
 
     if (wave) {
@@ -216,6 +354,123 @@ function detectCollision(modelMesh, otherMesh) {
     } else {
         return false;
     }
+}
+
+function updatePlayer(){
+    const angle = controls.getAzimuthalAngle()
+    
+    if (fwdValue > 0) {
+        tempVector.set(0, 0, -fwdValue).applyAxisAngle(upVector, angle)
+        walkModel.position.addScaledVector(tempVector, 1) 
+        walk.play()
+    }
+  
+    if (bkdValue > 0) {
+      tempVector.set(0, 0, bkdValue).applyAxisAngle(upVector, angle)
+      walkModel.position.addScaledVector(tempVector,1)
+      walk.play()
+    }
+
+    if (lftValue > 0) {
+      tempVector.set(-lftValue, 0, 0).applyAxisAngle(upVector, angle)
+      walkModel.position.addScaledVector(tempVector,1)
+      walk.play()
+    }
+
+    if (rgtValue > 0) {
+      tempVector.set(rgtValue, 0, 0).applyAxisAngle(upVector, angle)
+      walkModel.position.addScaledVector(tempVector,1)
+      walk.play()
+    }
+  
+  
+  // reposition camera
+  camera.position.sub(controls.target)
+  controls.target.copy(walkModel.position)
+  camera.position.add(walkModel.position)
+};
+
+function addJoystick(){
+   const options = {
+        zone: document.getElementById('joystickWrapper1'),
+        size: 120,
+        multitouch: true,
+        maxNumberOfNipples: 2,
+        mode: 'static',
+        restJoystick: true,
+        shape: 'circle',
+        position: { top: '60px', left: '60px' },
+        dynamicPage: true,
+        color: "black"
+      }
+   
+   
+  joyManager = nipplejs.create(options);
+  
+joyManager['0'].on('move', function (evt, data) {
+        const forward = data.vector.y
+        const turn = data.vector.x
+
+        if(walkModel !== undefined && mapModel !== undefined) {
+      if (detectCollision(walkModel, bridge1) === true && walkModel.position.y <= (bridge1.position.y + bridge1.geometry.boundingBox.max.y)) {
+        console.log("bridge 1")
+          walkModel.position.y = walkModel.position.y + (bridge1.position.y + bridge1.geometry.boundingBox.max.y) / 50; 
+      }
+      else if (detectCollision(walkModel, bridge2) === true && walkModel.position.y <= (bridge2.position.y + bridge2.geometry.boundingBox.max.y)) {
+        console.log("bridge 2")
+
+        walkModel.position.y = walkModel.position.y + (bridge2.position.y + bridge2.geometry.boundingBox.max.y) / 50; 
+      }
+      else if (detectCollision(walkModel, bridge3) === true && walkModel.position.y <= (bridge3.position.y + bridge3.geometry.boundingBox.max.y)) {
+        console.log("bridge 3")
+
+        walkModel.position.y = walkModel.position.y + (bridge3.position.y + bridge3.geometry.boundingBox.max.y) / 50; 
+      }
+      else if(detectCollision(walkModel, bridge1) === false && detectCollision(walkModel, bridge2) === false && detectCollision(walkModel, bridge3) === false && detectCollision(walkModel, bridge4) === true){
+        console.log("bridge 4")
+  
+        walkModel.position.y = walkModel.position.y - (bridge4.position.y + bridge4.geometry.boundingBox.max.y) / 40;
+      }
+      else if(detectCollision(walkModel, bridge1) === false && detectCollision(walkModel, bridge2) === false && detectCollision(walkModel, bridge3) === false && detectCollision(walkModel, bridge4) === false) {
+          walkModel.position.y = 6;
+      }
+
+      /*if(detectCollision(walkModel, arrowFrontModel) === true) {
+        console.log(detectCollision(walkModel, arrowFrontModel))
+        pointingArrowFront.stop();
+        arrowFrontModel.visible = false;
+      }
+      else {
+        arrowFrontModel.visible = true;
+        pointingArrowFront.play();
+      }*/
+    }
+
+        if (forward > 0) {
+          fwdValue = Math.abs(forward)
+          bkdValue = 0
+        } else if (forward < 0) {
+          fwdValue = 0
+          bkdValue = Math.abs(forward)
+        }
+
+        if (turn > 0) {
+          lftValue = 0
+          rgtValue = Math.abs(turn)
+        } else if (turn < 0) {
+          lftValue = Math.abs(turn)
+          rgtValue = 0
+        }
+      })
+
+     joyManager['0'].on('end', function (evt) {
+        bkdValue = 0
+        fwdValue = 0
+        lftValue = 0
+        rgtValue = 0
+        walk.stop()
+      })
+  
 }
 
 </script>
